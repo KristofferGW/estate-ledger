22bcd381a40387e6ec31b03158f285124528724a
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2023-08-31 17:18:49 +0200

    cleaned nodes.js

--- nodes.js
+++ nodes.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
-const {Blockchain} = require('./blockchain');
-const Block = require('./block');
 
 const app = express();
 const port = process.argv[2] || 3000;
 
 app.set('port', port);
 
-// const estateLedger = new Blockchain();
-
 app.use(bodyParser.json());
 
 app.get('/blocks', controller.getBlocks);
-// app.post('/mine', controller.mineBlock);
+
 app.post('/mine', (req, res) => {
     const currentNodePort = req.app.get('port');
     controller.mineBlock(req, res, currentNodePort);
